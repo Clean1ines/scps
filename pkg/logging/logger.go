db17a5009a4bb205d4f4a967291a791f8b6522bb
nullWave <100520276+Clean1ines@users.noreply.github.com>
2025-03-22 20:05:06 +0300

    Update logger.go

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -1,1 +1,22 @@
-g
+// pkg/logging/logger.go
+package logging
+
+import (
+	"cloud.google.com/go/logging"
+	"context"
+)
+
+// Logger – глобальный объект логгера.
+var Logger *logging.Logger
+
+// InitCloudLogger инициализирует клиента Cloud Logging для проекта.
+func InitCloudLogger(projectID string) (*logging.Logger, error) {
+	ctx := context.Background()
+	client, err := logging.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, err
+	}
+	// Создаем логгер с именем "scps"
+	Logger = client.Logger("scps")
+	return Logger, nil
+}
