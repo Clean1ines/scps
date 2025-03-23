b403e42b8f8750c17f7c786371cf2aaac661052e
nullWave <100520276+Clean1ines@users.noreply.github.com>
2025-03-23 19:56:20 +0300

    Update redis.go

--- pkg/storage/redis.go
+++ pkg/storage/redis.go
@@ -2,50 +2,23 @@
 package storage
 
 import (
-	"context"
-	"log"
-	"os"
-	"time"
+    "context"
+    "time"
 
-	"github.com/go-redis/redis/v8"
+    "github.com/go-redis/redis/v8"
 )
 
-var Client *redis.Client
-
-// InitRedis инициализирует подключение к Redis.
-func InitRedis() {
-	addr := os.Getenv("REDIS_ADDRESS")
-	if addr == "" {
-		log.Fatal("REDIS_ADDRESS не задан")
-	}
-	Client = redis.NewClient(&redis.Options{
-		Addr: addr,
-	})
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := Client.Ping(ctx).Err(); err != nil {
-		log.Fatalf("Ошибка подключения к Redis: %v", err)
-	}
-	log.Println("Redis подключен")
-}
-
-// SetValue сохраняет значение по ключу с заданным TTL.
-func SetValue(key string, value interface{}, ttl time.Duration) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	return Client.Set(ctx, key, value, ttl).Err()
-}
-
-// GetValue получает значение по ключу.
-func GetValue(key string) (string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	return Client.Get(ctx, key).Result()
-}
-
-// DelValue удаляет значение по ключу.
-func DelValue(key string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	return Client.Del(ctx, key).Err()
+// NewRedisClient создает клиент Redis и проверяет соединение.
+func NewRedisClient(address string) (*redis.Client, error) {
+    client := redis.NewClient(&redis.Options{
+        Addr:     address,
+        Password: "",
+        DB:       0,
+    })
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+    if err := client.Ping(ctx).Err(); err != nil {
+        return nil, err
+    }
+    return client, nil
 }
